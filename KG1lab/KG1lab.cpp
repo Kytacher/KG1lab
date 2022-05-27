c78fc2b5829e8ef23b1162542fbf0e477bd44de8
Maks <mrhellfyt@gmail.com>
2022-05-27 18:59:21 +0500

    Задания 16

--- KG1lab/KG1lab.cpp
+++ KG1lab/KG1lab.cpp
@@ -4,25 +4,37 @@
 #include <math.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include "glm/vec3.hpp"
-#include "glm/mat4x4.hpp"
-#include <list>
 #include <iostream>
 #include "Pipeline.h"
 #include "camera.h"
 #include "math_3d.h"
+#include "texture.h"
 
 #define ToRadian(x) ((x) * M_PI / 180.0f)
 #define ToDegree(x) ((x) * 180.0f / M_PI)
 
-#define WINDOW_WIDTH 1024
-#define WINDOW_HEIGHT 768
+#define WINDOW_WIDTH  1280
+#define WINDOW_HEIGHT 1024
 
+struct Vertex
+{
+	Vector3f m_pos;
+	Vector2f m_tex;
+
+	Vertex() {}
+
+	Vertex(Vector3f pos, Vector2f tex)
+	{
+		m_pos = pos;
+		m_tex = tex;
+	}
+};
 
 GLuint VBO;
-GLuint gWorldLocation;
+GLuint gWVPLocation;
 GLuint IBO;
-
+GLuint gSampler;
+Texture* pTexture = NULL;
 Camera* pGameCamera = NULL;
 
 // инициализация шейдера
@@ -30,22 +42,28 @@ static const char* pVS = "                                                      
 #version 330                                                                        \n\
                                                                                     \n\
 layout (location = 0) in vec3 Position;                                             \n\
+layout (location = 1) in vec2 TexCoord;                                             \n\
                                                                                     \n\
 uniform mat4 gWorld;                                                                \n\
                                                                                     \n\
+out vec2 TexCoord0;                                                                 \n\                                                                                    \n\
 void main()                                                                         \n\
 {                                                                                   \n\
-    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+     gl_Position = gWorld * vec4(Position, 1.0);                                      \n\
+    TexCoord0 = TexCoord;                                                           \n\
 }";
 
 static const char* pFS = "                                                          \n\
 #version 330                                                                        \n\
                                                                                     \n\
-out vec4 FragColor;                                                                 \n\
+in vec2 TexCoord0;                                                                  \n\
                                                                                     \n\
+out vec4 FragColor;                                                                 \n\
+uniform sampler2D gSampler;                                                         \n\
+                                                                                    \n\                                                                              \n\
 void main()                                                                         \n\
 {                                                                                   \n\
-    FragColor = vec4(0.4, 0.0, 1.0, 1.0);                                           \n\
+   FragColor = texture2D(gSampler, TexCoord0.xy);                                  \n\
 }";
 
 static void RenderSceneCB()
@@ -82,7 +100,7 @@ static void RenderSceneCB()
 	resize[3][0] = 0.0f; resize[3][1] = 0.0f; resize[3][2] = 0.0f; resize[3][3] = 1.0f;
 
 	glm::mat4x4 result = unit * rotate * move * resize;
-	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &result[0][0]); */
+	glUniformMatrix4fv(gWVPLocation, 1, GL_TRUE, &result[0][0]); */
 
 	//создаем объект конвейера, настраиваем его и отправляем результат в шейдер.
 	Pipeline p;
@@ -105,22 +123,27 @@ static void RenderSceneCB()
 
 //	p.SetCamera(pGameCamera->GetPos(), pGameCamera->GetTarget(), pGameCamera->GetUp());
 
-	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.getTransformation());
+	glUniformMatrix4fv(gWVPLocation, 1, GL_TRUE, (const GLfloat*)p.getTransformation());
 
 
 	//используем атрибуты вершин
 	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 	//привязка буфера для рисования
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//устанавливаем атрибуты вершин
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)12);
+	//привязка буфера для рисования
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 	//отрисовка
+	pTexture->Bind(GL_TEXTURE0);
 	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-	glDisableVertexAttribArray(0);
-//	for (int i = 0; i < 10; i++)
-//		glDrawArrays(GL_POLYGON, 0, i);
 	//отключаме атрбуты вершин
 	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+//	for (int i = 0; i < 10; i++)
+//		glDrawArrays(GL_POLYGON, 0, i);
 	//меняем местами фоновый буфер и буфер рендера местами
 	glutSwapBuffers();
 }
@@ -176,16 +199,13 @@ static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum Shad
 }
 
 void createVertexBuffer() {
-	glm::vec3 vertices[4];
-
-	vertices[0] = glm::vec3(-1.0f, -1.0f, 0.5773f);
-	vertices[1] = glm::vec3(0.0f, -1.0f, -1.15475f);
-	vertices[2] = glm::vec3(1.0f, -1.0f, 0.5773f);
-	vertices[3] = glm::vec3(0.0f, 1.0f, 0.0f);
-
+	Vertex Vertices[4] = { Vertex(Vector3f(-1.0f, -1.0f, 0.5773f), Vector2f(0.0f, 0.0f)),
+						   Vertex(Vector3f(0.0f, -1.0f, -1.15475), Vector2f(0.5f, 0.0f)),
+						   Vertex(Vector3f(1.0f, -1.0f, 0.5773f),  Vector2f(1.0f, 0.0f)),
+						   Vertex(Vector3f(0.0f, 1.0f, 0.0f),      Vector2f(0.5f, 1.0f)) };
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
 void createIndexBuffer() {
@@ -234,8 +254,10 @@ static void CompileShaders()
 	//glew использует шейдер
 	glUseProgram(ShaderProgram);
 	//сохроняем в переменную
-	gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
-	assert(gWorldLocation != 0xFFFFFFFF);
+	gWVPLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+	assert(gWVPLocation != 0xFFFFFFFF);
+	gSampler = glGetUniformLocation(ShaderProgram, "gSampler");
+	assert(gSampler != 0xFFFFFFFF);
 }
 
 
@@ -271,6 +293,10 @@ int main(int argc, char** argv)
 	}
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	glFrontFace(GL_CW);
+	glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
 	//фигурка
 	createVertexBuffer();
 	createIndexBuffer();
@@ -278,8 +304,15 @@ int main(int argc, char** argv)
 //	glGenBuffers(1, &VBO);
 //	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 //	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), &Vertices[0], GL_STATIC_DRAW);
-    
 	CompileShaders();
+	glUniform1i(gSampler, 0);
+
+	pTexture = new Texture(GL_TEXTURE_2D, "D://test.png");
+
+	if (!pTexture->Load()) {	
+		return 1;
+	}
+    
 	glutMainLoop();
 }
 
